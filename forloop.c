1c3db4df4ab461050fae8addcbf6ec5af0f9469e
nsrddyn@gmail.com <nsrddyn@gmail.com>
2024-10-25 22:42:24 +0200

    learned about defining types

--- forloop.c
+++ forloop.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
+#define lower 0
+#define upper 300
+#define step 20
 int main()
 {
-
      int fahr;
-     for (fahr = 0; fahr <= 300; fahr = fahr + 20)
+     for (fahr = lower; fahr <= upper; fahr = fahr + step)
           printf("%3d %6.3f\n", fahr, (5.0 / 9.0) * (fahr - 32));
+
+
+
 }

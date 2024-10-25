e9918f4f98c06027816b31792d832004e64217a8
nsrddyn@gmail.com <nsrddyn@gmail.com>
2024-10-25 21:52:31 +0200

    learned how for loops in c

--- celcius.c
+++ celcius.c
@@ -2,21 +2,18 @@
 
 int main()
 {
+    float fahr, celcius;
+    float lower, upper, step;
 
-   int celsius, fahr;
-   int lower, upper, step;
+    lower = 0;
+    upper = 300;
+    step = 20;
 
-   lower = 0;
-   upper = 300;
-   step = 20;
+    fahr = lower;
+    while (fahr <= upper) {
+        celcius = (5.0/9.0) * (fahr - 32.0);
+        printf("%3.0f %6.1f\n", fahr, celcius);
+        fahr = fahr + step;
+    }
 
-   fahr = lower;
-   while (fahr <= upper)
-   {
-      celsius = 5 * (fahr - 32) - 9;
-      printf("%d\t%d\n", fahr, celsius);
-      fahr = fahr + step;
-   }
-
-   return 0;
 }

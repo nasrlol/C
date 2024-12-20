21962287cafe1a65e8309c67adf2b583ab206c9b
Abdellah El Morabit <nsrddyn@gmail.com>
2024-12-21 00:03:49 +0100

    working a random password generator

---
+++ learningc/name.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int age;
+
+    char[] name;
+    char *name = NULL;
+    malloc(*name);
+
+    printf("%s", name);
+    printf("%d", age);
+
+    return 0;
+}
---
+++ learningc/passwordgen.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdio.h>
+
+char password[requested_size]()
+{
+
+    int index;
+    index = rand() % 10;
+    char alphabet[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+                       'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+    char random_generated_letter = sign[index];
+
+    return password;
+}
+
+int main()
+{
+
+    return 0;
+}
